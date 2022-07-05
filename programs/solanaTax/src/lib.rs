f6bd662dea55f5c78deafa19c07287511b0a5e23
Raja.Faizan.Nazir <Faizan.Nazir@geniTeam.com>
2022-07-05 13:06:59 +0500

    transfer

--- programs/solanaTax/src/lib.rs
+++ programs/solanaTax/src/lib.rs
@@ -1,33 +1,11 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Transfer, Token};
-//use anchor_spl::token;
-//use anchor_spl::token::{MintTo};
-//use solana_program::system_instruction::transfer;
-
 
 declare_id!("EP1LCMGBnETvbEZRkwUQh8ttjjtwLik7wAsV6eHWVYcj");
 
 #[program]
 pub mod token_contract {
     use super::*;
-
-    /*pub fn mint_token(ctx: Context<MintToken>,) -> Result<()> {
-        // Create the MintTo struct for our context
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.mint.to_account_info(),
-            to: ctx.accounts.token_account.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
-        };
-        
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        // Create the CpiContext we need for the request
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-
-        // Execute anchor's helper function to mint tokens
-        token::mint_to(cpi_ctx, 10)?;
-        
-        Ok(())
-    }*/
 
     pub fn transfer_token(ctx: Context<TransferToken>,ammount: u64) -> Result<()> {
         // Create the Transfer struct for our context
@@ -57,20 +35,6 @@ pub mod token_contract {
     }
 
 }
-
-/*#[derive(Accounts)]
-pub struct MintToken<'info> {
-    /// CHECK: This is the token that we want to mint
-    #[account(mut)]
-    pub mint: UncheckedAccount<'info>,
-    pub token_program: Program<'info, Token>,
-    /// CHECK: This is the token account that we want to mint tokens to
-    #[account(mut)]
-    pub token_account: UncheckedAccount<'info>,
-    /// CHECK: the authority of the mint account
-    #[account(mut)]
-    pub authority: AccountInfo<'info>,
-}*/
 
 #[derive(Accounts)]
 pub struct TransferToken<'info> {
